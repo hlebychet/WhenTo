aae038ed648f5229f0ef94aba60a6ad245ec925c
hlebychet <hlebychet@gmail.com>
2026-09-26 02:18:28 +0500

    chore: bump version to 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.autocalendar"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
     }
 
     signingConfigs {
